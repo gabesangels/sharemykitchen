5cb783620a552d349ade3db7ad5f0ccd331e69cc
Juan Gabriel <juangabriel@pixeloak.com>
2017-06-15 16:46:31 -0500

    Added hello page

---
+++ src/client/component/page/hello.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Message from '../../container/message'
+import HelloButton from '../../container/hello-button'
+
+const HelloPage = () => {
+  return (
+    <div>
+      <Message />
+      <HelloButton />
+    </div>
+  )
+}
+
+export default HelloPage
