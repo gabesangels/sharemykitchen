e17f066cc4b1d81c5995c03e0ca883f6fbb46d11
Juan Gabriel <juangabriel@pixeloak.com>
2017-06-15 16:49:47 -0500

    Added hello async page

---
+++ src/client/component/page/hello-async.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import MessageAsync from '../../container/message-async'
+import HelloAsyncButton from '../../container/hello-async-button'
+
+const HelloAsyncPage = () => {
+  return (
+    <div>
+      <MessageAsync />
+      <HelloAsyncButton />
+    </div>
+  )
+}
+
+export default HelloAsyncPage
