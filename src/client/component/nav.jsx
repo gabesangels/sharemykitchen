c95327ed08687e0a43cff3cd7a2f9e3e5501622b
Juan Gabriel <juangabriel@pixeloak.com>
2017-06-15 17:24:59 -0500

    Added Nav and tied up different pages to routes

---
+++ src/client/component/nav.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import {
+  HOME_PAGE_ROUTE,
+  HELLO_PAGE_ROUTE,
+  HELLO_ASYNC_PAGE_ROUTE,
+  NOT_FOUND_DEMO_PAGE_ROUTE,
+} from '../../shared/routes'
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        {[
+          { route: HOME_PAGE_ROUTE, label: 'Home' },
+          { route: HELLO_PAGE_ROUTE, label: 'Say Hello' },
+          { route: HELLO_ASYNC_PAGE_ROUTE, label: 'Say Hello Async' },
+          { route: NOT_FOUND_DEMO_PAGE_ROUTE, label: '404 Demo' },
+        ].map((link) => {
+          return (
+            <li key={link.route}>
+              <NavLink to={link.route} activeStyle={{ color: 'limegreen' }} exact>{link.label}</NavLink>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
--- src/shared/routes.js
+++ src/shared/routes.js
@@ -1,4 +1,4 @@
-export const HOME_PAGE_ROUTE = '/home'
+export const HOME_PAGE_ROUTE = '/'
 export const HELLO_PAGE_ROUTE = '/hello'
 export const HELLO_ASYNC_PAGE_ROUTE = '/hello-async'
 export const NOT_FOUND_DEMO_PAGE_ROUTE = '/404'
