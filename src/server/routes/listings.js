b2522dc780a84eb365046dfe8f76d5c15b26db95
Hank Bowen <hank@Ricks-MacBook-Air.local>
2017-06-20 10:45:17 -0500

    Wrote LISTINGS_DELETE route in server/routes/listings.js

--- src/server/routes/listings.js
+++ src/server/routes/listings.js
@@ -83,6 +83,26 @@ router.route(LISTINGS_UPDATE).put((req, res) => {
   })
 })
 
-router.route(LISTINGS_DELETE).delete((req, res) => {})
+router.route(LISTINGS_DELETE).delete((req, res) => {
+
+  //grab the listing id from the req.params
+  var id = req.params.id
+  //use findByIdAndRemove
+  ListingsModel.findByIdAndRemove(id, function(err, listing) {
+    //error handle
+    if (err) {
+      res.status(500).send(err)
+    }
+    //otherwise, send a confirmation back with a refernece 
+    var response = {
+      message: "Listing deleted",
+      id: id
+    }
+
+    res.send(response)
+    
+  })
+
+})
 
 export default router;
