22585d50f674dab5883f8951449f0c2fb8c5459e
Hank <hbowen1123@gmail.com>
2017-06-25 15:46:43 -0500

    Added more styles to listings page.

--- src/shared/component/listing.jsx
+++ src/shared/component/listing.jsx
@@ -6,21 +6,21 @@ import { listingsShowRoute } from '../routes'
 
 const Listing = (props) => {
   return (
-    <div className="card">
+    <div className="card mt-10 mr-10 ml-10 col-5">
       <Link
         className="one-listing"
         to={`${listingsShowRoute(props.kitchen._id)}`}
       >
-        <h2 className="card-header col-12">
+        <h3 className="card-header col-12">
           {props.kitchen.name}
-        </h2>
+        </h3>
         <div className="columns col-online">
           <img
-            className="listing-pic column center"
+            className="listing-pic card-image"
             src={props.kitchen.pictures[0]}
             alt="kitchen"
           />
-          <div className="card-body text-center column">
+          <div className="card-body text-center">
             <strong>Area:</strong> {props.kitchen.area} <br />
             <strong>Rating:</strong> {props.kitchen.rating} <br />
             <strong>Features:</strong>
